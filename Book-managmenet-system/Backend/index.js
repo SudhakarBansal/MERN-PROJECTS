75262184e4721202034b8908598f10441505183f
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2024-08-10 12:38:46 +0530

    backend-created

---
+++ Book-managmenet-system/Backend/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+const books = require('./db/Config')
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/fetch-data',async(req,res)=>{
+  let result = await books.find();
+  res.send(result);
+})
+
+app.post('/add-data',async(req,res)=>{
+  let book = new books(req.body);
+  let result = await book.save();
+  res.send(result);
+})
+
+app.put('/update-data/:id',async(req,res)=>{
+  try {
+    let result = await books.updateOne(
+      {_id : req.params.id},
+      {$set : req.body}
+    )
+    res.json(result);
+  } catch (err) {
+    res.status(500).send({error:err.message});
+  }
+})
+
+app.delete('/delete-data/:id', async (req, res) => {
+  try {
+    let result = await books.deleteOne({
+      _id: req.params.id
+    });
+    res.send(result)
+  } catch (error) {
+    res.send(error);
+  }
+})
+
+app.listen(5000,()=>{
+  console.log("server stated");
+})
