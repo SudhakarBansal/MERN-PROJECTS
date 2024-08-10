f699ca1d6e9ea47fb5d009f2b0ed48ebd3097cb4
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2024-08-10 15:00:30 +0530

    delete Functioanlity added

--- Book-managmenet-system/frontend/src/Components/Home.jsx
+++ Book-managmenet-system/frontend/src/Components/Home.jsx
@@ -12,8 +12,23 @@ const Home = () => {
   const getBooks = async () => {
     let result = await fetch("http://localhost:5000/fetch-data");
     result = await result.json();
-    console.log(result);
     setbooks(result);
+  }
+
+  const handleEdit=()=>{
+
+  }
+
+  const handleDelete=async(id)=>{
+    let result = await fetch(`http://localhost:5000/delete-data/${id}`,{
+      method:"DELETE"
+    });
+
+    result = await result.json();
+    if (result) {
+      getBooks();
+    }
+
   }
 
 
@@ -27,21 +42,22 @@ const Home = () => {
             books.map((book) => (
               <div className="card m-3" style={{ width: "22rem" }}>
                 {/* <img src="..." className="card-img-top" alt="..."/> */}
-                <div className="card-body justify-content-between">
-                  <div className="d-flex">
+                <div className="card-body">
+                  <div className="d-flex justify-content-between">
                   <h4 className="card-title">{book.title}</h4>
                   <h4 className="card-text"><span class="badge text-bg-secondary">${book.price}</span></h4>
                   </div>
                   <p className="card-text">{book.subTitle}</p>
                   <p className="card-text">Author : {book.author}</p>
-                  <button className='btn btn-outline-dark mx-1'>Edit</button>
-                  <button className='btn btn-outline-danger mx-1'>Delete</button>
+                  <button className='btn btn-outline-dark mx-1' onClick={handleEdit}>Edit</button>
+                  <button className='btn btn-outline-danger mx-1' onClick={()=>handleDelete(book._id)}>Delete</button>
                 </div>
               </div>
             ))
           }
         </div>
-      </div>) : <p>Loading...</p>}
+      </div>
+    ) : <p>Loading...</p>}
     </>
   )
 }
