2d8b1d75f72489ccf8c7ef2ebade81d22df9c3b4
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2024-08-12 12:06:32 +0530

    Edit Functionality added

--- Book-managmenet-system/frontend/src/App.js
+++ Book-managmenet-system/frontend/src/App.js
@@ -1,15 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 import AddData from './Components/AddData';
 import Home from './Components/Home'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [books, setbooks] = useState([]);
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home books={books} setbooks={setbooks}/>} />
           <Route path="/addEntry" element={<AddData/>} />
+          <Route path="/editEntry/:id" element={<AddData books={books}/>} />
         </Routes>
       </BrowserRouter>
     </>
--- Book-managmenet-system/frontend/src/Components/AddData.jsx
+++ Book-managmenet-system/frontend/src/Components/AddData.jsx
@@ -1,23 +1,50 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
 
-const AddData = () => {
+const AddData = ({ books }) => {
+  let { id } = useParams();
   const [title, settitle] = useState("");
   const [subTitle, setsubTitle] = useState("");
   const [author, setauthor] = useState("");
   const [price, setprice] = useState("")
+  const isEdit = id !== undefined;
+
+  useEffect(() => {
+    if (isEdit) {
+      const book = books.find((bk) => bk._id === id);
+      if (book) {
+        settitle(book.title);
+        setsubTitle(book.subTitle);
+        setauthor(book.author);
+        setprice(book.price);
+      }
+    }
+  }, [id, books])
+
 
   const navigate = useNavigate();
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    let result = await fetch("http://localhost:5000/add-data",{
-      method:"POST",
-      body:JSON.stringify({title,subTitle,author,price}),
-      headers:{
-        "Content-type":"application/json"
-      }
-    });
+    let result;
+
+    if (isEdit) {
+      result = await fetch(`http://localhost:5000/update-data/${id}`, {
+        method: "PUT",
+        body: JSON.stringify({ title, subTitle, author, price }),
+        headers: {
+          "Content-type": "application/json"
+        }
+      });
+    } else {
+      result = await fetch("http://localhost:5000/add-data", {
+        method: "POST",
+        body: JSON.stringify({ title, subTitle, author, price }),
+        headers: {
+          "Content-type": "application/json"
+        }
+      });
+    }
     console.log(result.json());
     navigate('/');
   }
@@ -25,26 +52,26 @@ const AddData = () => {
   return (
     <>
       <div className="container d-flex justify-content-center flex-column align-items-center py-5 text-light">
-        <h1>Add new Book</h1>
+        <h1>{isEdit ? ('Update') : ('Add New')} Book</h1>
         <form className='container py-5 w-50' onSubmit={handleSubmit}>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Title</label>
-            <input type="text" class="form-control bg-dark border-light text-light" id="exampleInputEmail1" aria-describedby="emailHelp" value={title} onChange={(e)=>settitle(e.target.value)}/>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Title</label>
+            <input type="text" className="form-control bg-dark border-light text-light" id="exampleInputEmail1" aria-describedby="emailHelp" value={title} onChange={(e) => settitle(e.target.value)} />
           </div>
-          <div class="mb-3">
-            <label for="exampleInputSubtitle1" class="form-label">Subtitle</label>
-            <input type="text" class="form-control bg-dark border-light text-light" id="exampleInputSubtitle1" value={subTitle} onChange={(e)=>setsubTitle(e.target.value)}/>
+          <div className="mb-3">
+            <label htmlFor="exampleInputSubtitle1" className="form-label">Subtitle</label>
+            <input type="text" className="form-control bg-dark border-light text-light" id="exampleInputSubtitle1" value={subTitle} onChange={(e) => setsubTitle(e.target.value)} />
           </div>
-          <div class="mb-3">
-            <label for="exampleInputSubtitle1" class="form-label">Author</label>
-            <input type="text" class="form-control bg-dark border-light text-light" id="exampleInputSubtitle1" value={author} onChange={(e)=>setauthor(e.target.value)}/>
+          <div className="mb-3">
+            <label htmlFor="exampleInputSubtitle1" className="form-label">Author</label>
+            <input type="text" className="form-control bg-dark border-light text-light" id="exampleInputSubtitle1" value={author} onChange={(e) => setauthor(e.target.value)} />
           </div>
-          <div class="mb-3">
-            <label for="exampleInputSubtitle1" class="form-label">Price</label>
-            <input type="text" class="form-control bg-dark border-light text-light" id="exampleInputSubtitle1" value={price} onChange={(e)=>setprice(e.target.value)}/>
+          <div className="mb-3">
+            <label htmlFor="exampleInputSubtitle1" className="form-label">Price</label>
+            <input type="text" className="form-control bg-dark border-light text-light" id="exampleInputSubtitle1" value={price} onChange={(e) => setprice(e.target.value)} />
           </div>
 
-          <button type="submit" class="btn btn-primary">Add Book</button>
+          <button type="submit" className="btn btn-primary">{isEdit ? ('Update') : ('Add')} Book</button>
         </form>
       </div>
     </>
